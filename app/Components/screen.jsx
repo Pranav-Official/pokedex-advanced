dcd02718f843531018ef6c75189e6c2ce461d389
Pranav-Official <pranavhtjp07@gmail.com>
2023-09-25 22:43:09 +0530

    the name cards and button layouts where completed,
    now fucntions need to be developed

--- app/Components/screen.jsx
+++ app/Components/screen.jsx
@@ -1,4 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -6,6 +8,9 @@ import 'swiper/css';
 import Image from 'next/image'
 
 const Screen = () => {
+
+
+
     return (
         <div className="screen border-0 flex-1 overflow-clip flex flex-row">
             <div className="Pokemon-display-card-container relative overflow-clip border-0 flex flex-col ">
@@ -16,22 +21,68 @@ const Screen = () => {
                     
                 </div>
 
-                <div className='Pokemon-display-card-box '>
+                <div className='Pokemon-display-card-box justify-center  '>
                     <div className='Pokemon-display-card bg-secondary-blue flex flex-col items-center justify-center '>
-                        <Image className='w-10/12 h-10/12' src="/base-sprites-compressed/24.png" width={300} height={300} />
+                        <Image className='pokemon-image ' src="/base-sprites-compressed/24.png" width={300} height={300} />
                     </div>
                     <div className='Pokemon-display-card bg-secondary-blue flex flex-col items-center justify-center '>
-                        <Image className='w-10/12 h-10/12' src="/base-sprites-compressed/25.png" width={300} height={300} />
+                        <Image className='pokemon-image ' src="/base-sprites-compressed/25.png" width={300} height={300} />
                     </div>
                     <div className='Pokemon-display-card bg-secondary-blue flex flex-col items-center justify-center '>
-                        <Image className='w-10/12 h-10/12' src="/base-sprites-compressed/26.png" width={300} height={300} />
+                        <Image className='pokemon-image ' src="/base-sprites-compressed/26.png" width={300} height={300} />
                     </div>
+                    
 
                 </div>
 
             </div>
-            <div className="name-card-container">
-                xcvbn
+            <div className="name-card-container flex flex-col  justify-center">
+                <div className='end-card flex flex-row '>
+                    <div className='basis-3/4 bg-menu-block'>
+
+                    </div>
+                    <div className='basis-1/4 bg-menu-block'>
+                        
+                    </div>
+                </div>
+                <div className='name-card-3-set flex flex-col'>
+                    <div className='name-card-sub bg-menu-block font-Chakra_Petch'>
+                        test
+                    </div >
+                    <div className='name-card-sub bg-menu-block font-Chakra_Petch'>
+                        test
+                    </div>
+                    <div className='name-card-sub bg-menu-block font-Chakra_Petch'>
+                        test
+                    </div>
+                    
+                </div>
+                <div className='name-card-main bg-menu-block font-Chakra_Petch'>
+                    
+                </div>
+                <div className='name-card-3-set flex flex-col'>
+                    <div className='name-card-sub bg-menu-block font-Chakra_Petch'>
+                        test
+                    </div >
+                    <div className='name-card-sub bg-menu-block font-Chakra_Petch'>
+                        test
+                    </div>
+                    <div className='name-card-sub bg-menu-block font-Chakra_Petch'>
+                        test
+                    </div>
+                    
+                </div>
+                <div className='end-card flex flex-row '>
+                    <div className='basis-3/4 bg-menu-block'>
+
+                    </div>
+                    <div className='basis-1/4 bg-menu-block'>
+                        
+                    </div>
+                </div>
+               
+
+                
             </div>
         </div>
     )

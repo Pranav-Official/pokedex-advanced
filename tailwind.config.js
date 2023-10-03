bcb4ef4256b45ee9f0c10d00c94c80f43fe27ed9
Pranav-Official <pranavhtjp07@gmail.com>
2023-10-03 22:50:20 +0530

    Search suggestions complete and extra animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     colors: {
-      'BG-green': '#7AC74F',
-      'primary-blue': '#05C8FF',
-      'primary-red': '#EC1313',
-      'secondary-blue': '#71E0FF',
-      'menu-block': '#FFC979',
+      "BG-green": "#7AC74F",
+      "primary-blue": "#05C8FF",
+      "primary-red": "#EC1313",
+      "secondary-blue": "#71E0FF",
+      "menu-block": "#FFC979",
       "pkemon-yellow": "#FFCC3C",
       "pkemon-blue": "#396BB7",
       "dpad-grey": "#3d3d3d",
-
-
     },
     fontFamily: {
       Just_Me_Again_Down_Here: ["Just Me Again Down Here"],
       Chakra_Petch: ["Chakra Petch", "sans-serif"],
-     },
-     extend: {
-      backgroundImage:{
-        'pokedex': 'url(/pokedex-copy.svg)',
-      }
-     },
+    },
+    extend: {
+      backgroundImage: {
+        pokedex: "url(/pokedex-copy.svg)",
+      },
+    },
   },
   plugins: [],
-}
+};
